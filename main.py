73e230ca2491707a99f149a78705908adece5fe6
veeoid <vc7410@rit.edu>
2026-04-22 12:48:31 -0500

    Added movement

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Collision Game")
 
-BG = pygame.transform.scale(
-    pygame.image.load("CollisionGame/game_bg.png"), (WIDTH, HEIGHT)
-)
+BG = pygame.transform.scale(pygame.image.load("game_bg.png"), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
+
+PLAYER_VEL = 8
 
 
 def draw(player):
@@ -28,12 +28,24 @@ def main():
     run = True
 
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if (
+            keys[pygame.K_RIGHT] or keys[pygame.K_d]
+        ) and player.x + PLAYER_VEL + PLAYER_WIDTH <= WIDTH:
+            player.x += PLAYER_VEL
+
         draw(player)
 
     pygame.quit()
